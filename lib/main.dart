788be147b9f375d5083e9d865e7abb34dca7c556
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-09-15 21:58:09 -0300

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:treinando/dao/transactions.dart';
 import 'package:treinando/utils/colors.dart';
 import 'home_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => Transactions(),
+        ),
+      ],
+      child: MyApp(),
+    ));
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'App treinamento',
       theme: ThemeData(
